8f9f7290a1cabe428948867568dd77e27cb1050f
Gp32Prog66 <szojo7@hotmail.com>
2024-04-01 20:11:32 -0700

    Fixed Game Logic

--- ppugames/src/App.js
+++ ppugames/src/App.js
@@ -62,21 +62,26 @@ const TicTacToe = () =>
     {
         const lines = 
         [
+        
+        //Verticlal
         [0,1,2],
         [3,4,5],
         [6,7,8],
+
+        //Horizontal
         [0,3,6],
         [1,4,7],
         [2,5,8],
+
+        //Diagonal
         [0,4,8],
-        [2,4,6],
-        [3,5,7]
+        [2,4,6]
         ];
 
         for (var i = 0; i < lines.length; i++)
         {
             const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[c])
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
             {
                 return squares[a];
             }
